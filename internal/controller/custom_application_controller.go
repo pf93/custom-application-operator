fee8de9d516458787e88e3b028322fdda05bce42
panfeng <panfeng@qiniu.com>
2024-06-01 16:53:59 +0800

    add deployment reconcile

---
+++ internal/controller/custom_application_controller.go
@@ -0,0 +1,144 @@
+/*
+Copyright 2024 pf93.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"reflect"
+	"time"
+
+	appsv1 "github.com/pf93/custom-application-operator/api/v1"
+	v1 "github.com/pf93/custom-application-operator/api/v1"
+	apiv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var (
+	CounterReconcileApplication	int64
+)
+
+const GenericRequeueDuration time.Duration = 1 * time.Minute
+
+
+// CustomApplicationReconciler reconciles a CustomApplication object
+type CustomApplicationReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=apps.pf93.cn,resources=customapplications,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps.pf93.cn,resources=customapplications/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=apps.pf93.cn,resources=customapplications/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the CustomApplication object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.0/pkg/reconcile
+func (r *CustomApplicationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	<- time.NewTicker(100 * time.Millisecond).C
+	log := log.FromContext(ctx)
+
+	CounterReconcileApplication += 1
+	log.Info("Starting a reconcile", "number", CounterReconcileApplication)
+	
+	app := &v1.CustomApplication{}
+	err := r.Get(ctx, req.NamespacedName, app)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("Application not found.")
+			return ctrl.Result{}, nil
+		}
+		log.Error(err, "Failed to get the Application, will requeue after a short time.")
+		return ctrl.Result{RequeueAfter: GenericRequeueDuration}, nil 
+	}
+	
+	var result ctrl.Result
+
+	result, err = r.ReconcileDeployment(ctx, app)
+	if err != nil {
+		log.Error(err, "Failed to reconcile Deployment")
+		return result, err
+	}
+
+	// TODO: reconcile service
+	return ctrl.Result{}, nil
+}
+
+func (r *CustomApplicationReconciler) ReconcileDeployment(ctx context.Context, app *v1.CustomApplication)(ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	dp := &apiv1.Deployment{}
+
+	err := r.Get(ctx, types.NamespacedName{
+		Namespace: app.Namespace,
+		Name: app.Name,
+	}, dp)
+	if err == nil {
+		log.Info("The Deployment has already exist.")
+		if reflect.DeepEqual(dp.Status, app.Status.Workflow) {
+			return ctrl.Result{}, nil
+		}
+
+		app.Status.Workflow = dp.Status
+		if err := r.Status().Update(ctx, app); err != nil {
+			log.Error(err, "Failed to update Application status")
+			return ctrl.Result{RequeueAfter: GenericRequeueDuration}, err
+		}
+		log.Info("The Application status has been updated.")
+		return ctrl.Result{}, nil
+	}
+
+	if !errors.IsNotFound(err) {
+		log.Error(err, "Failed to get Deployment, will requeue after a short time.")
+		return ctrl.Result{RequeueAfter: GenericRequeueDuration}, err
+	}
+
+	newDp := &apiv1.Deployment{}
+	newDp.SetName(app.Name)
+	newDp.SetNamespace(app.Namespace)
+	newDp.SetLabels(app.Labels)
+	newDp.Spec = app.Spec.Deployment.DeploymentSpec
+	newDp.Spec.Template.SetLabels(app.Labels)
+
+	if err := ctrl.SetControllerReference(app, newDp, r.Scheme); err != nil {
+		log.Error(err, "Failed to create Deployment, will requeue after a short time.")
+		return ctrl.Result{RequeueAfter: GenericRequeueDuration}, err 
+	}
+	
+	log.Info("The Deployment has been created.")
+	return ctrl.Result{}, nil
+}
+
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *CustomApplicationReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&appsv1.CustomApplication{}).
+		Complete(r)
+}
+
+
